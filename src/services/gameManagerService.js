2c8f650128724cdfed7086e31bae3751d692138b
marat2183 <marat.grig318@gmail.com>
2022-02-11 17:08:12 +0300

    add new services to gameManagerService and refactor services

--- src/services/gameManagerService.js
+++ src/services/gameManagerService.js
@@ -1,11 +1,17 @@
 import { blackFiguresFieldsInit, whiteFiguresFieldsInit, fieldsInit } from "fieldsData.js";
 import PlayerService from "./playerService";
+import PawnPredictionService  from "./predictionServices/pawnPredictionMoveService";
+import ValidationService from "./validationService";
+import FormatterService from "./formatterService";
+
 
 const GameManagerService = class {
-  constructor(blackFigurePlayer, whiteFigurePlayer, orderColor){
+  constructor(blackFigurePlayer, whiteFigurePlayer, orderColor, pawnPrediction){
     this.blackFigurePlayer = blackFigurePlayer;
     this.whiteFigurePlayer = whiteFigurePlayer;
     this.order = orderColor;
+    this.pawnPrediction = pawnPrediction;
+    this.availableFieldsToMove = null
   }
 
   toggleOrder = () => {
@@ -31,9 +37,32 @@ const GameManagerService = class {
   changePlayerFigurePosition = (newField) => {
     const player = this.getPlayerByOrder();
     //validation move
-    player.changeFigurePosition(newField)
-    player.resetSelectedFigureField()
+    const possibleFieldsToMove = this.getAvailableFieldsToMove();
+    console.log(possibleFieldsToMove);
+    player.changeFigurePosition(newField);
+    player.resetSelectedFigureField();
   }
+
+  getAvailableFieldsToMove = () => this.availableFieldsToMove;
+
+  setAvailableFieldsToMove = () => {
+    const selectedField = this.getPlayerSelectedField();
+    const fields = this.getAllFields();
+    let fieldsToMove = []
+    switch (selectedField.figure.type){
+      case 'pawn':
+        console.log(this.pawnPrediction)
+        fieldsToMove = this.pawnPrediction.getAvailableFieldsToMove(selectedField.figure, selectedField.fieldName, fields);
+        break;
+      default:
+    }
+    this.availableFieldsToMove = fieldsToMove;
+    return
+  }
+
+  resetAvailableFieldsToMove = () => {
+    this.availableFieldsToMove = null
+  };
 
   getFiguresFields = () => {
     const whiteFigures = this.whiteFigurePlayer.getFiguresFields();
@@ -42,17 +71,27 @@ const GameManagerService = class {
   }
 
   getAllFields = () => {
-    const defaultFields = fieldsInit;
+    //JSON.parse/JSON.stringify to deep clone
+    const defaultFields = JSON.parse(JSON.stringify(fieldsInit));
     const figuresFields = this.getFiguresFields();
+    const availableFieldsToMove = this.getAvailableFieldsToMove();
     const updatedFields = {...defaultFields, ...figuresFields}
+    if (availableFieldsToMove){
+      availableFieldsToMove.forEach(field => updatedFields[field].isAvailableToMove = true)
+    }
     return updatedFields;
   }
 }
 
 
-const whiteFigurePlayer = new PlayerService(whiteFiguresFieldsInit)
-const blackFigurePlayer = new PlayerService(blackFiguresFieldsInit)
+const whiteFigurePlayer = new PlayerService(whiteFiguresFieldsInit);
+const blackFigurePlayer = new PlayerService(blackFiguresFieldsInit);
 
-const gameManager = new GameManagerService(blackFigurePlayer, whiteFigurePlayer, 'white')
+const formatter = new FormatterService();
+const validation = new ValidationService(formatter);
+
+const pawnPrediction = new PawnPredictionService(validation, formatter);
+
+const gameManager = new GameManagerService(blackFigurePlayer, whiteFigurePlayer, 'white', pawnPrediction);
 
 export default gameManager;
--- src/services/playerService.js
+++ src/services/playerService.js
@@ -7,13 +7,13 @@ const PlayerService = class {
   getFiguresFields = () => this.figuresFieldsObj;
 
   changeFigurePosition = (newField) => {
-    const {fieldName, isActive, isBusy, figure} = this.selectedFigureField;
+    const {fieldName, isBusy, figure} = this.selectedFigureField;
 
     delete this.figuresFieldsObj[fieldName]
 
     this.figuresFieldsObj[newField.fieldName] = {
       fieldName: newField.fieldName,
-      isActive,
+      isSelected: false,
       isBusy,
       figure
     }
@@ -23,9 +23,13 @@ const PlayerService = class {
   getSelectedFigureField = () => this.selectedFigureField;
 
   setSelectedFigureField = (field) => {
+    console.log(field)
     const fieldsList = Object.values(this.figuresFieldsObj)
+    console.log(fieldsList.includes(field))
     if (fieldsList.includes(field)){
       this.selectedFigureField = field;
+      this.figuresFieldsObj[field.fieldName].isSelected = true;
+      console.log(this.figuresFieldsObj)
       return
     }
     throw new Error ('Its not your figure!')
